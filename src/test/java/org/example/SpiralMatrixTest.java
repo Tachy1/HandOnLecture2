32c1efee43dc3b3389135294c22a8a0a08a34454
Sterling Vieau <lordtachanka7771@gmail.com>
2025-02-06 12:05:25 -0800

    init commit

---
+++ src/test/java/org/example/SpiralMatrixTest.java
@@ -0,0 +1,14 @@
+package org.example;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.util.*;
+
+class SpiralMatrixTest {
+    @Test
+    public void testSpiralMatrix() {
+        SpiralMatrix sm = new SpiralMatrix();
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        List<Integer> expected = Arrays.asList(1, 2, 3, 6, 9, 8, 7, 4, 5);
+        assertEquals(expected, sm.spiralOrder(matrix));
+    }
+}
